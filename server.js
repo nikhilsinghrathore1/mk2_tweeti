9edbe1e69a68e8b1c5c58ec0a986f31e8716e60b
nikhilsinghrathore1 <111992033+nikhilsinghrathore1@users.noreply.github.com>
2025-06-23 14:39:29 +0530

    trying one more time

--- server.js
+++ server.js
@@ -156,12 +156,7 @@ async function handleNewCommits(payload) {
       });
     }
 
-    console.log("thank you god")
-    console.log("testing the hashtag functionality")
-    console.log("testing ")
-    console.log("one more testing after increasing the word limit ")
-    console.log("now testing the tag feature")
-    console.log("bajigar")
+    
     console.log("badsha")
 
     // Send to your backend/process the commit
